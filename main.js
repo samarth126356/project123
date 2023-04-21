8c9502d9189a7b1877e70de3b9a767d66bf87a93
samarth126356 <115333639+samarth126356@users.noreply.github.com>
2023-04-21 17:39:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -10,12 +15,26 @@ function setup(){
 }
 function draw(){
     background('#969A97');
+    
+    document.getElementById("font_size").innerHTML = "Font-size of the text will be = " + difference + "px";
+    textSize(difference);
+    fill('#F90093');
+    text('Samarth',50,400);
+    
 }
 function modelLoaded(){
     console.log('PoseNet Is Initialized!');
 }
-function gotPoses(){
+function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        noseX= results[0].pose.nose.x;
+        noseY= results[0].pose.nose.y;
+        console.log("noseX = " + noseX + " noseY = " + noseY);
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+        console.log("leftWristX =" + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
+
     }
 }
